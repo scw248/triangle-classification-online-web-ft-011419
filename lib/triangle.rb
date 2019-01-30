097b04d476909152c7c5907c15fc0d058fa6a15d
Stephen Wright <scw248@gmail.com>
2019-01-30 17:24:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
   
   def kind
     
-    if (length1 + length2 =< length3) || (length1 + length3 =< length2) || (length2 + length3 =< length1) || (length1 == 0 || length2 == 0 || length3 == 0)
+    if (length1 + length2 <= length3) || (length1 + length3 <= length2) || (length2 + length3 <= length1) || (length1 == 0 || length2 == 0 || length3 == 0)
       raise TriangleError
     elsif length1 == length2 && length2 == length3
       :equilateral
