5a78f7d640b7f0dd6ff96bbf53ab4168cfa30b92
Stephen Wright <scw248@gmail.com>
2019-01-30 17:11:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,8 +11,10 @@ class Triangle
   def kind
     
     
-    class TriangleError < StandardError
-    # triangle error code
+    
   end
+  
+  class TriangleError < StandardError
+    # triangle error code
   end
 end
