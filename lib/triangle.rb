9ddc1dbf3b84705fba77118c8badc649d9cbab48
Stephen Wright <scw248@gmail.com>
2019-01-30 17:24:37 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :length1, :length2, :length3
+  
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
+  
+  def kind
+    
+    if (length1 + length2 <= length3) || (length1 + length3 <= length2) || (length2 + length3 <= length1) || (length1 == 0 || length2 == 0 || length3 == 0)
+      raise TriangleError
+    elsif length1 == length2 && length2 == length3
+      :equilateral
+    elsif (length1 == length2 && length2 != length3) || (length2 == length3 && length3 != length1) || (length1 == length3 && length1 != length2)
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  
+  class TriangleError < StandardError
+    "No side can be less than a length of 1 AND two sides added together must always be greater than the third side"
+  end
 end
